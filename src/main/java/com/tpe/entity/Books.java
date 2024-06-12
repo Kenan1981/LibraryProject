f816cb762b3974fa66d7ce2f7a6dc78de1958566
Kenan <kenanuyanik@gmail.com>
2024-06-12 22:19:06 +0300

    v02

---
+++ src/main/java/com/tpe/entity/Books.java
@@ -0,0 +1,12 @@
+package com.tpe.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Books {
+
+    @Id
+    Long id;
+}
