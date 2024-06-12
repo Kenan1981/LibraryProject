6ddd46584d3669af88e4367e73d80a1b37b43cd2
Kenan <kenanuyanik@gmail.com>
2024-06-12 22:23:10 +0300

    v04

--- src/main/java/com/tpe/entity/Books.java
+++ src/main/java/com/tpe/entity/Books.java
@@ -2,9 +2,12 @@ package com.tpe.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Books {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
 }
