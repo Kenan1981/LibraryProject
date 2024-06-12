b0b2cf4e65ca0248aad1d0b119838dbf01028c5a
Kenan <kenanuyanik@gmail.com>
2024-06-12 21:43:08 +0300

    V01

---
+++ src/main/java/com/tpe/LibrayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrayProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpe/LibrayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
