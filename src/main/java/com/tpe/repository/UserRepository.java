ae2fa35b0df867f664e64a51acc1d57380bdbb23
Kenan <kenanuyanik@gmail.com>
2024-06-13 21:21:03 +0300

    v06

---
+++ src/main/java/com/tpe/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tpe.repository;
+
+import com.tpe.entity.concretes.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
