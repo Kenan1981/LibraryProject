9e9e8c566b680aaf179bce9e72ebf00aad9b3cd2
Veli Kara <113050906+velikkom@users.noreply.github.com>
2024-06-13 21:45:27 +0300

    User.java entity

--- src/main/java/com/tpe/entity/concretes/user/User.java
+++ src/main/java/com/tpe/entity/concretes/user/User.java
@@ -8,8 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -22,7 +26,7 @@ import java.util.List;
 @ToString
 public class User {
 
-    @Id
+/*    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -42,5 +46,58 @@ public class User {
     private List<UserRole> roles;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Loan> loans;*/
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String lastName;
+
+    @NotNull
+    @Min(value = -2)
+    @Max(value = 2)
+    private Integer score = 0;
+
+    @NotNull
+    @Size(min = 10, max = 100)
+    private String address;
+
+    @NotNull
+    @Pattern(regexp = "\\d{3} \\d{3} \\d{4}")
+    private String phone;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy MM dd")
+    private Date birthDate;
+
+    @NotNull
+    @Email
+    @Size(min = 10, max = 80)
+    private String email;
+
+    @NotNull
+    private String password; // Bu alan şifrelenmiş (hashed) olmalıdır
+
+    @NotNull
+    private LocalDateTime createDate;
+
+    private String resetPasswordCode; // Bu alan hashlenmiş olmalıdır
+
+    @NotNull
+    private Boolean builtIn = false;
+
+    @OneToMany(mappedBy = "user")
     private List<Loan> loans;
+
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<UserRole> userRoles;
+
+
 }
