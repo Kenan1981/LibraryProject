df1c8a08ffc5db0bde0c74709d5a33cb667b8f0c
Sefa Eyer <eyersefa@gmail.com>
2024-06-14 20:28:08 +0300

    CategoryD2

---
+++ src/main/java/com/tpe/controller/CategoryController.java
@@ -0,0 +1,12 @@
+package com.tpe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+}
