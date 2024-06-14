a4cbd047a53d41461ebe21cd7f53e5f0c2708023
Busra Ozer <busraucar1994@gmail.com>
2024-06-13 22:46:32 -0400

    Author Repo-Ser-Contr added

---
+++ src/main/java/com/tpe/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.tpe.repository;
+
+import com.tpe.entity.concretes.business.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
